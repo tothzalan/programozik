cfbfd844957a8c7dfcdb47bb408b01e1035ccfc9
tothzalan <toth.zalan03@gmail.com>
2023-08-22 18:30:21 +0200

    smol thingies

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -13,17 +13,18 @@
                     <b class="text-xl">Link:</b> <a
                         href="{{ str_contains($project->link, '//') ? $project->link : '//' . $project->link }}">{{ $project->link }}</a>
                     @auth
-                        <div>
+                        <div class="pb-5">
                             <button class="mt-6">
                                 <a class="bg-yellow-300 rounded-full p-3" href="/projects">Go back</a>
                             </button>
                         </div>
                     @endauth
-                    <br><br>
                     <hr>
 
+                    </form>
+                    <h2 class="font-bold text-xl">Comments</h2><br>
                     {{-- comment section --}}
-                    <br>
+                    @auth
                     <h2 class="font-bold text-xl">Add comment</h2>
                     <form method="post" action="{{ route('comments.store') }}">
                         @csrf
@@ -37,15 +38,21 @@
                                 value="Add Comment" />
                         </div><br>
                         <hr>
-                    </form>
-                    {{-- TODO: show comments in the right order --}}
-                    <h2 class="font-bold text-xl">Comments</h2><br>
-                    @foreach ($project->comments as $comment)
-                        <div class="border text-left p-2">
-                            <div class="font-bold">{{ $comment->user->name }}</div>
-                            <div>{{ $comment->body }}</div>
-                        </div>
-                    @endforeach
+                    @else
+                        <h2 class="font-bold text-xl text-center">
+                            <a href="{{ route('login') }}" class="text-blue-500">Log in</a> to comment
+                        </h2>
+                    @endauth
+
+                    @if(count($project->comments) > 0)
+                        {{-- TODO: show comments in the right order --}}
+                        @foreach ($project->comments as $comment)
+                            <div class="border text-left p-2">
+                                <div class="font-bold">{{ $comment->user->name }}</div>
+                                <div>{{ $comment->body }}</div>
+                            </div>
+                        @endforeach
+                    @endif
                 </div>
             </div>
         </div>
