32edb47f3a209d4b52401eda8b6f18242a9ecc50
sancirigo <sancirigo@gmail.com>
2023-09-11 20:07:21 +0200

    implement upvoting of posts

--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Comment::class);
     }
+
+    public function upvotes()
+    {
+        return $this->hasMany(Upvote::class);
+    }
 }
