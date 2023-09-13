9272787b70c03d38cf102e267776d35925e6a03c
sancirigo <sancirigo@gmail.com>
2023-09-13 19:53:27 +0200

    change title to programozik

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>ProgramozIK</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,7 +12,7 @@
 
         <!-- Styles -->
         @vite('resources/css/app.css')
-    
+
     </head>
     <body class="antialiased">
         <div class="">
