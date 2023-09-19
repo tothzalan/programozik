cd64cbdea9853fd4222df6f908eb62b823798d59
tothzalan <toth.zalan03@gmail.com>
2023-09-19 22:17:32 +0200

    kinda working Mailing

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -7,7 +7,9 @@ use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-
+use Mailgun\Mailgun;
+use Exception;
+use Illuminate\Support\Facades\Log;
 
 class MembersController extends Controller
 {
@@ -22,6 +24,23 @@ class MembersController extends Controller
         
         $membership = new Member(['project_id' => $projectId, 'user_id' => Auth::user()->id, 'valid' => false]);
         $membership->save();
+
+        $project = Project::find($projectId);
+        
+
+        try {
+            $mg = Mailgun::create(env('MAILGUN_KEY'));
+
+            $mg->messages()->send(env('MAILGUN_DOMAIN'), [
+                'from' => 'no-reply@' . env('MAILGUN_DOMAIN'),
+                'to' => $project->owner->email,
+                'subject' => 'New join request from ' . Auth::user()->name . ' in ' . $project->name . '!',
+                'text' => 'You have received a new join request from ' . Auth::user()->name . ' in project ' . $project->name,
+            ]);
+
+        } catch(Exception $exception) {
+            Log::error('Set up correct environmental variables to use Mailgun');
+        }
 
         return Redirect::route('projects.show', $projectId);
     }
